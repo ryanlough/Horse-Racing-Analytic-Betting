0766528d12f778d8bb0f97d45d5e8f3977e81bb6
Ryan Lough <ryanjlough@gmail.com>
2015-08-23 22:51:12 -0400

    Horse owner/trainer added. Now MULTITHREADING!!

--- Horse.cs
+++ Horse.cs
@@ -7,20 +7,34 @@ using System.Threading.Tasks;
 
 namespace HorseRacing
 {
+  enum Position
+  {
+    None,
+    Win,
+    Place,
+    Show,
+    Fourth
+  }
   class Horse
   {
     private int number;
-    private String name;
-    private String jockey;
+    private Position position;
+    private string name;
+    private string jockey;
     private double odds;
+    private string trainer;
+    private string owner;
 
     //Private constructor for Horse
-    private Horse(string number, String name, String jockey, string odds)
+    private Horse(string number, Position position, string name, string jockey, string odds, string trainer, string owner)
     {
       this.number = Convert.ToInt32(number.Trim());
+      this.position = position;
       this.name = name;
       this.jockey = jockey;
       this.odds = Convert.ToDouble(odds.Trim());
+      this.trainer = trainer.Trim();
+      this.owner = owner.Trim();
     }
 
     public static Horse[] getHorses(string s)
@@ -34,8 +48,22 @@ namespace HorseRacing
         int i = 0;
         foreach (Match match in matches)
         {
-          horses[i++] = new Horse(match.Groups["num"].Value, match.Groups["name"].Value,
-                             match.Groups["jockey"].Value, match.Groups["odds"].Value);
+          Position pos;
+          switch (i) {
+            case 0: pos = Position.Win;
+              break;
+            case 1: pos = Position.Place;
+              break;
+            case 2: pos = Position.Show;
+              break;
+            case 3: pos = Position.Fourth;
+              break;
+            default: pos = Position.None;
+              break;
+          }
+          horses[i++] = new Horse(match.Groups["num"].Value, pos, match.Groups["name"].Value,
+                             match.Groups["jockey"].Value, match.Groups["odds"].Value, 
+                             getTrainer(s, match.Groups["num"].Value), getOwner(s, match.Groups["num"].Value));
         }
         return horses;
       }
@@ -51,13 +79,35 @@ namespace HorseRacing
      */
     private static MatchCollection getHorseData(string s)
     {
-      string pattern = @"\s(?<num>\d)\s(?<name>(\D+\s)?(\S+\s))\((?<jockey>(\S+,\s\S+)+)\).+?(?<odds>\d+\.\d+)";
+      string pattern = @"\s(?<num>\d+)\w?\s(?<name>(\D+\s)?(\S+\s))\((?<jockey>(\S+,\s\S+)+)\).+?(?<odds>\d+\.\d+)";
       return Regex.Matches(s.Substring(s.IndexOf("Last Raced")), pattern, RegexOptions.ExplicitCapture);
+    }
+
+    private static string getTrainer(string s, string number)
+    {
+      string pattern = @"\s" + number.Trim() + @"\w?\s-\s(?<trainer>(\S+,(\s\S+,)?\s\S+)+)\s";
+      foreach (Match match in Regex.Matches(s.Substring(s.IndexOf("Trainers:")), pattern, RegexOptions.ExplicitCapture))
+      {
+        return match.Groups["trainer"].Value.Replace(';', ' ');
+      }
+      return "";
+    }
+
+    private static string getOwner(string s, string number)
+    {
+      string pattern = @"\s?" + number.Trim() + @"\w?\s?-\s?(?<owner>\D+)\d";
+      //Hack to fix retrival last owner: Replace Footnotes with 0
+      foreach (Match match in Regex.Matches(s.Substring(s.IndexOf("Owners:")).Replace("Footnotes", "0"), pattern, RegexOptions.ExplicitCapture))
+      {
+        return match.Groups["owner"].Value.Replace(';', ' ');
+      }
+      return "";
     }
 
     public override string ToString()
     {
-      return "Number: " + number + " Name: " + name + " Jockey: " + jockey + " Odds: " + odds;
+      return "Number: " + number + " Position: " + position + " Name: " + name +
+             " Jockey: " + jockey + " Odds: " + odds + " Trainer: " + trainer + " Owner: " + owner;
     }
 
 
--- Program.cs
+++ Program.cs
@@ -6,22 +6,26 @@ using System.Threading.Tasks;
 using iTextSharp;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using System.Threading;
 
 
 namespace HorseRacing
 {
+  /**
+   * Main class
+   */
   class Program
   {
     public static int currentRace = 0;
     public static string[] sbArray = new string[11];
     static void Main(string[] args)
     {
-      DateTime t = new DateTime(2005, 8, 13);
+      DateTime dateTime = new DateTime(2005, 8, 13);
 
-      string zeroMonth = t.Month < 10 ? "0" : "";
-      string zeroDay   = t.Day < 10 ? "0" : "";
+      string zeroMonth = dateTime.Month < 10 ? "0" : "";
+      string zeroDay   = dateTime.Day < 10 ? "0" : "";
 
-      string date = zeroMonth + t.Month + "/" + zeroDay + t.Day + "/" + t.Year;
+      string date = zeroMonth + dateTime.Month + "/" + zeroDay + dateTime.Day + "/" + dateTime.Year;
       
       //PdfReader reader = new PdfReader(@"http://www.equibase.com/premium/eqbPDFChartPlus.cfm?RACE=A&BorP=P&TID=SAR&CTRY=USA&DT=" + date + "&DAY=D&STYLE=EQB");
       PdfReader reader = new PdfReader(@"C:\Users\Ryan\test.pdf");
@@ -37,24 +41,17 @@ namespace HorseRacing
         processor.ProcessContent(ContentByteUtils.GetContentBytesForPage(reader, x), resourcesDic);
       }
 
-      Race[] races = new Race[11];
-      int i = 0;
-      
-      foreach (String s in sbArray) {
-        //Abort if the race has too few characters.
-        //Workaround due to equibase putting a ton of redundant text in their pdfs for no reason...
-        if (s == null || s.Length < 150) {
-          break;
-        }
-        races[i++] = new Race(i, Race.getPurse(s), Horse.getHorses(s), Race.getWeather(s), Race.getTrack(s), Race.getLength(s));
-      }
+      DataHandler handler = new DataHandler(dateTime, sbArray);
+      Thread thread = new Thread(new ThreadStart(handler.extractPdfData));
 
-      Day d = new Day(t, races);
-
-      Console.WriteLine(d.ToString());
-      Console.ReadLine();
+      thread.Start();
+      thread.Join();
+      while (true) ;
     }
 
+    /**
+     * Class to convert a PDF into usable text.
+     */
     public class SBTextRenderer : IRenderListener
     {
 
@@ -89,6 +86,40 @@ namespace HorseRacing
       }
 
       #endregion
+    }
+
+    /**
+     * Class to handle reading and storing all the extracted PDF data.
+     */
+    public class DataHandler
+    {
+      private DateTime date;
+      private string[] pages;
+
+      public DataHandler(DateTime date, string[] pages)
+      {
+        this.date = date;
+        this.pages = pages;
+      }
+
+      public void extractPdfData()
+      {
+        Race[] races = new Race[11];
+        int i = 0;
+
+        foreach (String s in sbArray)
+        {
+          //Abort if the race has too few characters.
+          //Workaround due to equibase putting a ton of redundant text in their pdfs for no reason...
+          if (s == null || s.Length < 150)
+          {
+            break;
+          }
+          races[i++] = new Race(i, Race.getPurse(s), Horse.getHorses(s), Race.getWeather(s), Race.getTrack(s), Race.getLength(s));
+        }
+
+        Console.WriteLine(new Day(date, races).ToString());
+      }
     }
   }
 }
